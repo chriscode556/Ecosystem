558c9045c59e3dabd10ca17aa79e86063ecc2154
chriscode556 <88249968+chriscode556@users.noreply.github.com>
2021-07-31 14:13:15 -0500

    Update Ecosystem.java

--- Ecosystem.java
+++ Ecosystem.java
@@ -1,32 +1,4 @@
-/*
- * Project Requirements (Requirements Specification)
- * 1. Select an ecosystem to simulate.
- * 		1a. The Ecosystem must be unique with regard to other classmates
- * 		1b. The Ecosystem must be diverse enough to be able to create simulations
- * 		1c. Take notes about the ecosystem and how it works. Include weather considerations 
- * 			food availability, seasonality, etc.
- * 2. Identify at least 5 animals to simulate in your ecosystem
- * 		2a. The animals must be chosen with regard to their interactions with each other
- * 		2b. The animals must all be active at the same time (night vs day vs twilight)
- * 		2c. Take notes about how the animals behave with regard to their species, 
- * 			predators or prey and competition with other species for scarce resources
- * 3. Tdentify at least two simulations to create
- *		3a. Each simulation must involve at least two species
- * 
- * Design Specification
- * 1.	Ecosystem
- * 		1a. EcosystemType must be unique in class.
- * 		1b. Your ecosystem may be given any name but must specify the unique EcosystemType
- * 		1c. Your ecosystem class must derive from this abstract class Ecosystem. 
- * 			The tech lead has sole responsibility for this class
- * 		1d.	Do not include a main() method. Your ecosystem must be called from outside the class
- * 2.	Animal
- * 		2a. The animal classes must derive from the Animal class in our hierarchy
- * 		2b. EcosystemType must be specified in each class
- * 		2c. Age related behaviors must be created for eating, moving and sleeping
- * 
- * 		
- */
+
 
 package christ.core_java.common.ecosystem;
 
